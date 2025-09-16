aad77a8c9fc7dadfd95c4f5e717361715a6edd07
NZ888 <nikon.05.04.2009@gmail.com>
2025-09-16 23:24:40 +0200

    added promo

--- src/components/main page/MainPageComponent.tsx
+++ src/components/main page/MainPageComponent.tsx
@@ -1,31 +1,40 @@
 import React from 'react';
-import styles from './MainPage.module.css'
-interface MainPageComponentProps {
-  
-}
+import styles from './MainPage.module.css';
+import promoImage from '../../assets/images/promo/image 1.png'
 
-const MainPageComponent: React.FC<MainPageComponentProps> = ({  }) => {
-  return (
-    <div className={styles.container}>
-        <div className={styles.catContainer}>
-          <div>
-            <h2>CATEGORIES</h2>
-          </div>
-          <div className={styles.categoriesDiv}>
+const MainPageComponent: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.catContainer}>
+                <h2>CATEGORIES</h2>
+                <div className={styles.categoriesDiv}>
+                    <p>Clothes</p>
+                    <p>Shoes</p>
+                    <p>Furniture</p>
+                    <p>Cosmetics</p>
+                    <p>Travel</p>
+                    <p>Automotive</p>
+                </div>
+            </div>
 
-            <p>Clothes</p>
-            <p>Shoes</p>
-            <p>Furniture</p>
-            <p>Cosmetics</p>
-            <p>Travel</p>
-            <p>Automotive</p>
-          </div>
+            <div className={styles.promoContainer}>
+                <div className={styles.salePromoDiv}>BIG SALE 20%</div>
+                <div className={styles.promoContent}>
+                    <div className={styles.promoDescriptionDiv}>
+                        <p>the bestseller of 2022</p>
+                        <h3>
+                            LENNON r2d2 <br />
+                            with NVIDIA 5090 TI
+                        </h3>
+                        <button>Shop Now</button>
+                    </div>
+                    <div className={styles.promoImageDiv}>
+                        <img src={promoImage} alt="promo" />
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className={styles.promoContainer}>
-          2
-        </div>
-    </div>
-  );
+    );
 };
 
 export default MainPageComponent;
